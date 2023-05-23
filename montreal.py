c97ac9267bc04601c307f687da6040351f9a7f60
Sandro Ferroni <sandro.ferroni@epita.fr>
2023-05-23 19:36:17 +0200

    parcours drone

--- montreal.py
+++ montreal.py
@@ -1,7 +1,35 @@
 import osmnx as ox
 import matplotlib.pyplot as plt
+import networkx as nx
+import copy
 
 city = ox.graph_from_place('Montréal, Canada', network_type='all')
 
 ox.plot_graph(city)
 plt.show()
+ 
+#marche taille neoud bien
+def ParcourirArretes(graphe):
+    visites = set()  # Ensemble pour stocker les arêtes déjà visitées
+    graphe_modifie = copy.deepcopy(graphe)  # Copie du graphe initial
+
+    for u, v, data in graphe_modifie.edges(data=True):
+        if 'color' not in data or data['color'] == 'skyblue':
+            visiter_arrete(u, v)
+            nx.set_edge_attributes(graphe_modifie, {(u, v, None): {'color': 'red'}})  # Changer la couleur de l'arête visitée
+            #print(graph[u][v])
+
+    dessiner_graphe(graphe_modifie)
+
+def visiter_arrete(sommet, voisin):
+    print(f"Visite de l'arête entre {sommet} et {voisin}")
+
+def dessiner_graphe(graphe):
+    pos = nx.spring_layout(graphe)
+    edge_colors = [data.get('color', 'skyblue') for _, _, data in graphe.edges(data=True)]
+    nx.draw(graphe, pos, with_labels=False, node_size=2, node_color='red', edge_color=edge_colors, width=2)
+
+    plt.show()
+
+
+ParcourirArretes(city)
--- outremont.py
+++ outremont.py
@@ -3,42 +3,32 @@ import matplotlib.pyplot as plt
 import networkx as nx
 
 graph = ox.graph_from_place("Outremont, Montréal, Canada", network_type='all')
-"""graph_und = graph.to_undirected()
-graph_simpl = nx.Graph()
-for u,v, key, data in graph_und.edges(keys=True, data=True):
-    graph_simpl.add_edge(u,v,**data)
-drone_path = [1234,5678,9101,1121]
-fig,  ax =ox.plot_graph(graph_simpl, show=False, close=False, figsize=(10,10))
-ox.plot_graph(graph_simpl, drone_path, route_linewidth=6, route_color='r', route_alpha=0.7, ax=ax)"""
 
 ox.plot_graph(graph)
 plt.show()
+import copy
 
-def parcours_rapide(graph):
-    queue = []
-    visited = {node:False for node in graph.nodes()}
-    first_node = list(graph.nodes())[0]
-    queue.append(list(graph.nodes())[0])
-    visited[first_node] = True
-    graph_display = graph.copy()
-    while queue :
-        current_node = queue.pop(0)
+def ParcourirArretes(graphe, sommet, visites,i): #parcours profondeur 
+    visites.add(sommet)  # Ajouter le sommet à l'ensemble des visites
+    voisins = graphe.neighbors(sommet)  # Récupérer les voisins du sommet
+    for voisin in voisins:
+        if voisin not in visites:  # Vérifier si le voisin n'a pas été visité
+            visiter_arrete(sommet, voisin)
+            ParcourirArretes(graphe, voisin, visites,1)
+            nx.set_edge_attributes(graphe, {(sommet, voisin, None): {'color': 'red'}})  # Changer la couleur de l'arête visitée
+            visiter_arrete(voisin,sommet)
+    if (i==0):
+        dessiner_graphe(graphe)
 
-        graph_display.nodes[current_node]["visited"] = True
+def visiter_arrete(sommet, voisin):
+    print(f"Chemain entre {sommet} et {voisin}")
 
-        #print("Noeux :", current_node)
-        for neighbor in graph.neighbors(current_node):
-            if not visited[neighbor]:
-                queue.append(neighbor)
-                visited[neighbor] = True
+def dessiner_graphe(graphe):
+    pos = nx.spring_layout(graphe)
+    edge_colors = [data.get('color', 'skyblue') for _, _, data in graphe.edges(data=True)]
+    nx.draw(graphe, pos, with_labels=False, node_size=2, node_color='red', edge_color=edge_colors, width=2)
 
-                graph_display.add_edge(current_node,neighbor)
-
-    print("Parcours termine !")
-
-    node_color = ["black" if graph_display.nodes[node].get("visited") else "blue" for node in graph_display.nodes()]
-    edge_color = ["red" if graph_display.nodes[node].get("visited") else "gray" for node in graph_display.nodes()]
-    nx.draw(graph_display,with_labels=False, node_color=node_color, edge_color=edge_color, node_size=10)
     plt.show()
 
-parcours_rapide(graph)
+graphe_modifie = copy.deepcopy(graph)
+ParcourirArretes(graphe_modifie, list(graphe_modifie.nodes)[0], set(),0)
